696a3fe286cac8471f70b1b39b19ec85e87c1af1
ha quang le <winlp4ever@gmail.com>
2020-05-17 23:29:32 +0200

    rm no longer needed files

--- nlp/test.py
+++ nlp/test.py
@@ -1,43 +1,9 @@
-import json
-import time
+import yake 
 
-from elasticsearch import Elasticsearch
-import psycopg2
-import psycopg2.extras
-import uuid
+text = 'comment faire un tableau en css'
 
+kw_extractor = yake.KeywordExtractor()
+keywords = kw_extractor.extract_keywords(text)
 
-
-es = Elasticsearch()
-
-
-# delete the old index in case it already exists
-es.indices.delete(index='qa', ignore=[400, 404])
-# and create a new one
-with open('./nlp/index.json') as index_file:
-    source = index_file.read().strip()
-    es.indices.create(index='qa', body=source)
-
-# connect to database
-f = open('db-credentials/config.json') # load config file
-dbconfig = json.load(f) # load credentials details
-conn = psycopg2.connect("dbname=%s user=%s host=%s port=%d password=%s"
-    % (dbconfig['database'], dbconfig['user'], dbconfig['host'], dbconfig['port'], dbconfig['password']))
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) # use psycopg2.extras.DictCursor to return row as dict object
-# retrieve all questions
-cur.execute("SELECT * FROM question;")
-q = cur.fetchone()
-while q:
-    time.sleep(0.1)
-    doc = {
-        'id': q['id'],
-        'text': q['question_text'],
-        'vectorisation': q['vectorisation']
-    }
-    print(q['question_text'])
-    es.index(index='qa', body=doc, id=q['id'])
-    print('done for %d' % q['id'])
-    q = cur.fetchone()
-# close the database
-cur.close()
-conn.close()
+for kw in keywords:
+	print(kw)
