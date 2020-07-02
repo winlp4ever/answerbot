72bff43c19e9350a35211a86044a93f1590b110a
NathanGodey <38216711+NathanGodey@users.noreply.github.com>
2020-07-02 17:42:22 +0200

    Update tokenizer.py

--- nlp/tokenizer.py
+++ nlp/tokenizer.py
@@ -5,7 +5,7 @@ from nltk.tokenize.stanford import StanfordTokenizer
 import re
 import os
 
-SNLP_TAGGER_JAR = "/home/redlcamille/workspace/bob/nlp/stanford-postagger.jar"
+SNLP_TAGGER_JAR = "./stanford-postagger.jar"
 
 
 def tokenize(tknzr, sentence, to_lower=True):
