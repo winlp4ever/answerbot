f2f26bd994a1f1ef546b93af7c6847515d99c5b0
Ubuntu <ubuntu@ip-172-31-10-227.eu-west-3.compute.internal>
2020-12-23 09:36:14 +0000

    fix bugs

--- nlp/qa-server.py
+++ nlp/qa-server.py
@@ -10,7 +10,7 @@ import requests
 import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://bob3wa:@theai_bob3wa_2020@test-db.czcdgzwouwz1.eu-west-3.rds.amazonaws.com:5433/dbbob3wa"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://bob3wa:@theai_bob3wa_2020@db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com:5432/dbbob3wa"
 db = SQLAlchemy(app)
 res = Responder()
 
